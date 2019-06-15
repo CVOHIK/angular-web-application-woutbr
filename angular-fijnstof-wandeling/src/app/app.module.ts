1acc397ae6aa30b76b073a346e0bcce82a6895ca
woutbr <woutbr@users.noreply.github.com>
2019-06-15 16:49:04 +0200

    Angular Google Map: libraries definieren in array

--- angular-fijnstof-wandeling/src/app/app.module.ts
+++ angular-fijnstof-wandeling/src/app/app.module.ts
@@ -14,11 +14,12 @@ import { MyErrorHandler } from './my-error-handler';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { RoutemapComponent } from './routemap/routemap.component';
-import { HomeComponent } from './home/home.component';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
+import { RoutemapComponent } from './routemap/routemap.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,11 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     BrowserModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({
-      apiKey: environment.apiKeyGMaps + "&libraries=places&libraries=visualization"
+      apiKey: environment.apiKeyGMaps,
+      libraries: [
+        'places',
+        'visualization'
+      ]
     }),
     BrowserAnimationsModule,
     MaterialModule,
